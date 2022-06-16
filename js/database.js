f184fe689609da7fec200eb29ef7be3955c0039c
Jake Stephens <jake@pop-os.localdomain>
2022-06-16 08:57:44 -0400

    stringify

--- js/database.js
+++ js/database.js
@@ -1,12 +1,12 @@
 function insert_message() {
   var xmlHttp = new XMLHttpRequest();
   xmlHttp.open("POST", "https://bitfoxtech.com/foxyapi/insert_message", false);
-  data = {
+  var data = JSON.stringify({
     "user_name": document.getElementById("cust_name").value,
     "user_email": document.getElementById("cust_email").value,
     "category": document.getElementById("product").value,
     "body": document.getElementById("message_body").value
-  }
+  })
   console.log(data);
   xmlHttp.send(data);
   console.log(xmlHttp.responseText)
