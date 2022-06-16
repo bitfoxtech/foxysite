d9d3b27183724e83642580e1ecfac5aa59c60522
Jake Stephens <jake@pop-os.localdomain>
2022-06-16 08:45:07 -0400

    pulling info from dom fields

--- js/database.js
+++ js/database.js
@@ -1,7 +1,13 @@
 function send_message() {
   var xmlHttp = new XMLHttpRequest();
-  xmlHttp.open("GET", "https://bitfoxtech.com/foxyapi/", false);
-  xmlHttp.send(null);
+  xmlHttp.open("POST", "https://bitfoxtech.com/foxyapi/send_message", false);
+  data = {
+    "user_name": document.getElementById("cust_name").innerText,
+    "user_email": document.getElementById("cust_email").innerText,
+    "category": document.getElementById("product").innerText,
+    "body": document.getElementById("message_body").innerText
+  }
+  xmlHttp.send(data);
   console.log(xmlHttp.responseText)
   return xmlHttp.responseText;
 }
