25d49e536820cb8a4c7348ecc38b4d59de6e6209
Jake Stephens <jake@pop-os.localdomain>
2022-06-16 08:56:11 -0400

    value

--- js/database.js
+++ js/database.js
@@ -2,10 +2,10 @@ function insert_message() {
   var xmlHttp = new XMLHttpRequest();
   xmlHttp.open("POST", "https://bitfoxtech.com/foxyapi/insert_message", false);
   data = {
-    "user_name": document.getElementById("cust_name").innerText,
-    "user_email": document.getElementById("cust_email").innerText,
-    "category": document.getElementById("product").innerText,
-    "body": document.getElementById("message_body").innerText
+    "user_name": document.getElementById("cust_name").value,
+    "user_email": document.getElementById("cust_email").value,
+    "category": document.getElementById("product").value,
+    "body": document.getElementById("message_body").value
   }
   console.log(data);
   xmlHttp.send(data);
